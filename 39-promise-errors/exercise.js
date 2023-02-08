b11b111e1bff8a3621be6fb13791d7999edbfd09
AndreaPossidente <ndrpssdnt@gmail.com>
2023-02-08 12:33:43 +0100

    done exercise 39

---
+++ 39-promise-errors/exercise.js
@@ -0,0 +1,28 @@
+const isLogged = true;
+
+function firstPromise(isLogged) {
+  return new Promise((resolve, reject) => {
+    if (!isLogged) {
+      reject(new Error("User's not logged in"));
+    }
+    resolve(Math.random());
+  });
+}
+
+function secondPromise(num) {
+  return new Promise((resolve, reject) => {
+    if (num <= 0.5) {
+      reject(new Error("num is less than 0.5"));
+    }
+    resolve(`{name: "John", age: 24}`);
+  });
+}
+
+firstPromise(isLogged)
+  .then((num) => secondPromise(num))
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err))
+  .finally(console.log("Hello from finally method!"));
+
+// praticamente lo avevo già fatto nell'esercizio precedente
+// ho aggiunto solo il finally.
